83a4fe3d19d856b11400f10135148a0cc7b2ed6c
Ankit Raj <ankit188raj@gmail.com>
2026-08-06 00:41:45 +0530

    Latest

--- Learn the basics/5_Introduction_to_Recursion.cpp
+++ Learn the basics/5_Introduction_to_Recursion.cpp
@@ -1,6 +1,21 @@
+/*
+Recursion - When a function calls itself until a specified condition is met.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int cnt=0;
 
+void f(){
+    if(cnt==4){
+        return;
+    }
+    cout<<cnt<<endl;
+    cnt++;
+    f();
+}
+
+int main(){
+    f();
 }
