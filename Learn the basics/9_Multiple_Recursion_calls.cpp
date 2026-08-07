7ccd314b408b18f4448e4008193b265aacc78865
Ankit Raj <ankit188raj@gmail.com>
2026-08-07 22:35:05 +0530

    Latest

---
+++ Learn the basics/9_Multiple_Recursion_calls.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Fibonacci number using multiple recursion calls
+int f(int n){ // O(2^n)
+    if(n<=1){
+        return n;
+    }
+    int last=f(n-1);
+    int s_last=f(n-2);
+    return last+s_last; // f(n)=f(n-1)+f(n-2)
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    int a=f(n);
+    cout<<a<<endl;
+
+
+}
