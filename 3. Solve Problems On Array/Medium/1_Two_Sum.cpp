297a2a6450afb02159b8464141f83803f14720bc
Ankit Raj <ankit188raj@gmail.com>
2026-08-31 23:31:06 +0530

    Latest

---
+++ 3. Solve Problems On Array/Medium/1_Two_Sum.cpp
@@ -0,0 +1,18 @@
+/*
+Two Sum : Check if a pair with given sum exists in Array
+
+Problem Statement: Given an array of integers arr[] and an integer target.
+1st variant: Return YES if there exist two numbers such that their sum is equal to the target. Otherwise, 
+return NO.
+2nd variant: Return indices of the two numbers such that their sum is equal to the target. Otherwise, we 
+will return {-1, -1}.
+
+Input: N = 5, arr[] = {2,6,5,8,11}, target = 14
+Output : YES
+Explanation: arr[1] + arr[3] = 14. So, the answer is “YES” for first variant for second variant output will be : [1,3].
+
+Input: N = 5, arr[] = {2,6,5,8,11}, target = 15
+Output : NO.
+Explanation: There exist no such two numbers whose sum is equal to the target. 
+*/
+
