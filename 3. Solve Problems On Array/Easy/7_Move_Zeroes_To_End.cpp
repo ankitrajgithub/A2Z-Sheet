52fa5687318bf2d10aed1a243450046b69cb8fac
Ankit Raj <ankit188raj@gmail.com>
2026-09-02 00:20:50 +0530

    Latest

--- 3. Solve Problems On Array/Easy/7_Move_Zeroes_To_End.cpp
+++ 3. Solve Problems On Array/Easy/7_Move_Zeroes_To_End.cpp
@@ -18,7 +18,35 @@ Explanation : All the zeros are moved to the end and non-negative integers are m
 using namespace std;
 
 void moveZeroesToEnd(int arr[],int n){
+    vector<int> temp;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            temp.push_back(arr[i]);
+        }
+    }
+    for(int i=0;i<temp.size();i++){
+        arr[i]=temp[i];
+    }
 
+    for(int i=temp.size();i<n;i++){
+        arr[i]=0;
+    }
+}
+
+void MoveZeroesToEnd(int arr[],int n){
+    int j=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            j=i;
+            break;
+        }
+    }
+    for(int i=j+1;i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
 }
 
 int main(){
@@ -30,7 +58,9 @@ int main(){
         cin>>arr[i];
     }
 
-    moveZeroesToEnd(arr,n);
+    // moveZeroesToEnd(arr,n);  Time Complexity-O(N+n+(N-n))=O(2N) Space Complexity-O(N)
+
+    MoveZeroesToEnd(arr,n); // Time Complexity=O(n+(N-n))=O(N) Space Complexity-O(1)
 
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
